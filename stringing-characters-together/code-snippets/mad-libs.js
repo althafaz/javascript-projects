c69474326152407a871ad96957ee51ced2cf57f8
althafaz <52558189+althafaz@users.noreply.github.com>
2024-01-16 20:06:13 -0500

    Exercises completed

--- stringing-characters-together/code-snippets/mad-libs.js
+++ stringing-characters-together/code-snippets/mad-libs.js
@@ -1,7 +1,7 @@
-let pluralNoun = ;
-let name = ;
-let verb = ;
-let adjective = ;
-let color = ;
+let pluralNoun = "runtime";
+let name_ = "numbers";
+let verb = "convert" ;
+let adjective = "logic";
+let color = "red";
 
-console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
+console.log(`JavaScript provides a ${color} collection of tools — including ${adjective} syntax and ${pluralNoun} — that allows ${name_} to ${verb} with strings.`)
