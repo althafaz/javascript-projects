d90c3a2f3633425202ace5fd3df4cb0e4363cca8
althafaz <52558189+althafaz@users.noreply.github.com>
2024-01-30 10:44:02 -0500

    Studio assignment partially done

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -7,4 +7,42 @@ let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
 
-console.log(/* your code here */);
+
+let newArrayForLoop = []
+
+function findMinNum(numOfArr) {
+
+    let minNum = numOfArr[0]
+    for(let i= 1; i < numOfArr.length;i++){
+    
+        if(numOfArr[i] < minNum){
+            minNum = numOfArr[i]
+        }
+    
+        else{
+            return minNum
+        }
+
+        console.log(minNum)
+    }
+}
+
+
+let newArray = []
+let minvalues = (arrOfNum)=>{return newArray.push(arrOfNum.sort((a,b) =>{ return a-b}).shift())}
+
+function makeItOrder(arrOfNum){
+
+    if(arrOfNum.length === 0){
+        console.log("New Array:"+ newArray)
+    }
+
+    else{
+        minvalues(arrOfNum)
+        return makeItOrder(arrOfNum)
+    }
+}
+
+
+console.log(makeItOrder(nums2))
+
