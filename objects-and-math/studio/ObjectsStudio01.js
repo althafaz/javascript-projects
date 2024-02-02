778ddaa9a3e395cfe942fad14a2b93293aaa04fb
althafaz <52558189+althafaz@users.noreply.github.com>
2024-02-02 00:24:16 -0500

    ObjStudio01 completed

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,8 +1,69 @@
 // Code your selectRandomEntry function here:
+function selectRandomEntryTwo(animalsList) {
+
+  let randomIdx = Math.floor(Math.random() * animalsList.length);
+  return ([animals[randomIdx].astronautID, randomIdx]);
+}
+
+
+function selectRandomEntry(ids) {
+
+  let randomIdx = Math.floor(Math.random() * ids.length);
+  return ids[randomIdx];
+}
+
+
+
+function drawThreeRandomNumbersTwo(animalsList){
+  let luckyAnimalsList = []
+
+    let [randomNumber,idx] = selectRandomEntryTwo(animalsList)
+
+  while(luckyAnimalsList.length < 3){
+
+    if(luckyAnimalsList.includes(animals[idx])){
+      [randomNumber,idx] = selectRandomEntryTwo(animalsList)
+    }
+    else{
+      console.log(randomNumber,idx)
+          luckyAnimalsList.push(animals[idx])
+    }
+  }
+
+  return luckyAnimalsList;
+}
+
+let recursionRandom = []
+function selectThreeRecursion(ids){
+
+  let id = selectRandomEntry(ids)
+  if(recursionRandom.length == 3){
+    return recursionRandom
+  }
+
+  if(recursionRandom.includes(id)){
+    return selectThreeRecursion(ids)
+  }
+  else{
+    recursionRandom.push(id);
+    return selectThreeRecursion(ids)
+  }
+}
 
 
 // Code your buildCrewArray function here:
-
+function buildCrewArray(idx,candidateObj){
+  let luckyCrew = []
+  for( id in idx){
+      for(candidate of candidateObj){
+        if(candidate.astronautID == idx[id]){
+          luckyCrew.push(candidate)
+        }
+      }
+    }
+  
+    return luckyCrew;
+}
 
 let idNumbers = [291, 414, 503, 599, 796, 890];
 
@@ -53,3 +114,13 @@ let candidateF = {
 let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
 
 // Code your template literal and console.log statements:
+let luckyWinners = buildCrewArray(selectThreeRecursion(idNumbers),animals)
+console.log(`${luckyWinners[0].name}, ${luckyWinners[1].name}, and ${luckyWinners[2].name} are going to space!`)
+
+// for(let i = 0; i < 5;i++)
+// {
+//   console.log(drawThreeRandomNumbers(animals));
+// }
+
+//console.log(selectThreeRecursion(idNumbers));
+//console.log(buildCrewArray(selectThreeRecursion(idNumbers),animals))
