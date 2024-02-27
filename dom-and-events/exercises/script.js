b16dab90de6aa50cc68e53fea93af2888c0f107e
althafaz <52558189+althafaz@users.noreply.github.com>
2024-02-27 00:33:01 -0500

    Exercise completed

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,23 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click',e=>{
+        paragraph.innerHTML = "Houston, we have liftoff!"
+    })
+
+    missionAbort.addEventListener('mouseover',e=>{
+        e.target.style.backgroundColor = "red"
+    })
+
+    missionAbort.addEventListener('mouseout',e=>{
+        e.target.style.backgroundColor = ""
+    })
+
+    missionAbort.addEventListener('click',e=>{
+        if(window.confirm("Are you sure you want to abort the mission?") === true){
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home"
+        }
+    })
 }
 
 window.addEventListener("load", init);
