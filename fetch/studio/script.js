9e8e3eb24724efa907d2fa0c9e3f459e05461462
althafaz <52558189+althafaz@users.noreply.github.com>
2024-03-04 23:11:27 -0500

    Studio completed

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,40 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function(){
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(res=>{
+        if(res.ok){
+            res.json().then((data)=>{
+                data.sort((a, b)=>{
+                   if(a.hoursInSpace < b.hoursInSpace) {
+                        return-1
+                    }
+                    if (a.hoursInSpace > b.hoursInSpace) {
+                        return 1
+                    } 
+                    if (a.hoursInSpace === b.hoursInSpace) {
+                        return 0
+                    }
+                })
+                const container = document.getElementById("container")
+                let listAstronut = ''
+                data.forEach(el => {
+                    listAstronut += `
+                                <div class="astronaut">
+                                    <div class="bio">
+                                        <h3>${el.firstName +' '+ el.lastName} </h3>
+                                            <ul>
+                                                <li>Hours in space: ${el.hoursInSpace}</li>
+                                                <li>Active: ${el.active ? 'is active' : 'is not active'}</li>
+                                                <li>Skills: ${el.skills}</li>
+                                            </ul>
+                                    </div>
+                                    <img class="avatar" src=${el.picture}>
+                                </div>`
+                    console.log(el)
+                })
+                container.innerHTML = listAstronut
+            })
+        }
+    })
+
+})
